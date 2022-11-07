e6edaf43210fc5b9ee0faa9ce85f1391e542bebb
徐珑璁 <ricole@trashMac.lan>
2022-11-07 00:15:42 -0500

    set all env and run

--- project/flask-tutorial/flaskr/server.py
+++ project/flask-tutorial/flaskr/server.py
@@ -3,9 +3,14 @@
 """
 Columbia W4111 Intro to databases
 Example webserver
+
 To run locally
+
     python server.py
+
 Go to http://localhost:8111 in your browser
+
+
 A debugger such as "pdb" may be helpful for debugging.
 Read about it online.
 """
@@ -17,6 +22,8 @@ from flask import Flask, request, render_template, g, redirect, Response
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
+
+
 
 # XXX: The Database URI should be in the format of: 
 #
@@ -59,6 +66,7 @@ def before_request():
   This function is run at the beginning of every web request 
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request
+
   The variable g is globally accessible
   """
   try:
@@ -97,18 +105,22 @@ def teardown_request(exception):
 def index():
   """
   request is a special object that Flask provides to access web request information:
+
   request.method:   "GET" or "POST"
   request.form:     if the browser submitted a form, this contains the data in the form
   request.args:     dictionary of URL arguments e.g., {a:1, b:2} for http://localhost?a=1&b=2
+
   See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
   """
 
   # DEBUG: this is debugging code to see what request looks like
   print(request.args)
+
+
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT email FROM Users")
+  cursor = g.conn.execute("SELECT name FROM test")
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
@@ -142,12 +154,11 @@ def index():
   #
   context = dict(data = names)
 
-
   #
   # render_template looks in the templates/ folder for files.
   # for example, the below file reads template/index.html
   #
-  return render_template("index.html")
+  return render_template("index.html", **context)
 
 #
 # This is an example of a different path.  You can see it at
@@ -157,20 +168,19 @@ def index():
 # notice that the functio name is another() rather than index()
 # the functions for each app.route needs to have different names
 #
-
 @app.route('/another')
 def another():
   return render_template("anotherfile.html")
 
 
 # Example of adding new data to the database
-# @app.route('/add', methods=['POST'])
-# def add():
-#   name = request.form['name']
-#   print(name)
-#   cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
-#   g.conn.execute(text(cmd), name1 = name, name2 = name);
-#   return redirect('/')
+@app.route('/add', methods=['POST'])
+def add():
+  name = request.form['name']
+  print(name)
+  cmd = 'INSERT INTO test(name) VALUES (:name1), (:name2)';
+  g.conn.execute(text(cmd), name1 = name, name2 = name);
+  return redirect('/')
 
 
 @app.route('/login')
@@ -191,14 +201,18 @@ if __name__ == "__main__":
     """
     This function handles command line parameters.
     Run the server using
+
         python server.py
+
     Show the help text using
+
         python server.py --help
+
     """
 
     HOST, PORT = host, port
     print("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+    app.run(host=HOST, port=PORT, debug=True, threaded=threaded)
 
 
   run()
